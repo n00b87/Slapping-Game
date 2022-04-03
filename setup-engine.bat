75bbdf0b2d5d165ff25a43187074124fe7d10560
n00b87 <n00bskillz87@gmail.com>
2022-04-02 21:59:19 -0500

    Setup Script

--- setup-engine.bat
+++ setup-engine.bat
@@ -1,1 +1,11 @@
+SET C_PATH=%CD%
 IF NOT EXIST %~dp0n00b-Engine MKLINK /J n00b-Engine %~dp0..\n00b-Engine
+cd %C_PATH%\..\n00b-Engine
+MKLINK /J bkg %C_PATH%\bkg
+MKLINK /J font %C_PATH%\font
+MKLINK /J gfx %C_PATH%\gfx
+MKLINK /J music %C_PATH%\music
+MKLINK /J sfx %C_PATH%\sfx
+MKLINK /J sprite %C_PATH%\sprite
+MKLINK /J stage %C_PATH%\stage
+MKLINK /J tile %C_PATH%\tile
